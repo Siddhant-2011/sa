36de9878caad1ac75adc8a7f1fea10ece2e4fd4f
Siddhant-2011 <74121489+Siddhant-2011@users.noreply.github.com>
2021-09-21 18:53:16 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+img="";
+function setup(){
+canvas=createCanvas(380,380);
+video=createCapture(VIDEO);
+video.size(380,380);
+video.hide();
+canvas.center();
+
+
+}
+
+function draw(){
+    img = loadImage('baby1.jpeg');
+}
